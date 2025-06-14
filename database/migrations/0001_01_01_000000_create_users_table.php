3d2956ad77f406c6c2cdfb49393d383b6d97bb15
Abdessamed <moulai.khatir.abd@gmail.com>
2025-06-14 16:44:41 +0100

    missed that users table already exist

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('age');
+            $table->string('country');
+            $table->enum('gender', ['male', 'female']);
             $table->rememberToken();
             $table->timestamps();
         });
