f6efdc7505898f22dad7d8a69bdeb744359d1318
Abdessamed <moulai.khatir.abd@gmail.com>
2025-06-16 12:21:46 +0100

    user_adkar pivot table migration

---
+++ database/migrations/2025_06_16_110504_create_user_adkar_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_adkar', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('adkar_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');;
+            $table->foreign('adkar_id')->references('id')->on('adkar')->onDelete('cascade');;
+
+            // $table->foreignId('user_id')->constrained();    keep on mind
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_adkar');
+    }
+};
