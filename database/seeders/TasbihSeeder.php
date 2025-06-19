be2a39121cf8d173964df0428080f7f38a026149
Abdessamed <moulai.khatir.abd@gmail.com>
2025-06-19 11:42:20 +0100

    generating seeder for tasbihs table

---
+++ database/seeders/TasbihSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Tasbih;
+use App\Models\User;
+
+class TasbihSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Tasbih::factory(5)->create();
+
+        foreach (Tasbih::all() as $tasbih) {
+            $users = User::inRandomOrder()->take(rand(1,5))->pluck('id');
+            $tasbih->users()->attach($users);
+        }
+    }
+}
