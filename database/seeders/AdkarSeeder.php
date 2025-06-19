3324f04f95f5ebc854adc453e7b2c26af3fbee12
Abdessamed <moulai.khatir.abd@gmail.com>
2025-06-19 14:02:13 +0100

    understanding random take pluck ids :) look to Seeders comments

--- database/seeders/AdkarSeeder.php
+++ database/seeders/AdkarSeeder.php
@@ -16,10 +16,10 @@ class AdkarSeeder extends Seeder
     {
         Adkar::factory(20)->create();
 
-        foreach (Adkar::all() as $adkar) {
-            $users = User::inRandomOrder()->take(rand(1,20))->pluck('id');
+        foreach (Adkar::all() as $adkar) {  // this line iterate on all the 20 adkar created
+            $users = User::inRandomOrder()->take(rand(1,20))->pluck('id');  // here we get a random number of users ID (e.g 3 user_id)
             foreach ($users as $userId) {
-                $adkar->users()->attach($userId, ['date' => now()]);
+                $adkar->users()->attach($userId, ['date' => now()]);  // attach the current dikr with those 3 users ID in pivot table
             }
         }
     }
--- database/seeders/TasbihSeeder.php
+++ database/seeders/TasbihSeeder.php
@@ -16,10 +16,10 @@ class TasbihSeeder extends Seeder
     {
         Tasbih::factory(5)->create();
 
-        foreach (Tasbih::all() as $tasbih) {
-            $users = User::inRandomOrder()->take(rand(1,5))->pluck('id');
+        foreach (Tasbih::all() as $tasbih) {  // this line iterate on all the 20 tasbih created
+            $users = User::inRandomOrder()->take(rand(1,5))->pluck('id');  // here we get a random number of users ID (e.g 3 user_id)
             foreach ($users as $userId) {
-                $tasbih->users()->attach($userId, ['date' => now()]);
+                $tasbih->users()->attach($userId, ['date' => now()]);   // attach the current tasbih with those 3 users ID in pivot table
             }
         }
     }
