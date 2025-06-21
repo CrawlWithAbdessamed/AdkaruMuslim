47b5f4f76f962d8793e52003d6e88f089887f4f3
Abdessamed <moulai.khatir.abd@gmail.com>
2025-06-21 19:06:33 +0100

    store adkars function implemented

--- app/Http/Controllers/Api/AdkarController.php
+++ app/Http/Controllers/Api/AdkarController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Adkar;
+use Illuminate\Support\Facades\Auth;
 
 class AdkarController extends Controller
 {
@@ -21,8 +22,14 @@ class AdkarController extends Controller
 
     public function store(Request $request)
     {
-        $name = $request->input('name');
- 
+        $request->validate(['adkar_id' => 'required|exists:adkars,id']);
 
+        $user = Auth::user();
+
+        $adkar = Adkar::findOrFail($request->adkar_id);
+
+        $user->adkars()->attach($adkar->id, ['completed' => true]);
+
+        return response()->json(['message' => 'Adkar completed successfully']);
     }
 }
